00b4e73c8d87e5b90a659a33dbd5124bbaef86ee
hopepisode <hopepisode@gmail.com>
2023-10-31 02:21:15 +0900

    각 전시회의 메인 설명 화면 추가 로고 이미지 메인 홈페이지에 추가 및 그에 맞춰 검색창이나 로그인 버튼 크기 수정 아직 검색창에서 조회 기능 없음 네비바에서 추천전시회를 입력하면 detail페이지로 이동

---
+++ server/auth/db.js
@@ -0,0 +1,10 @@
+// MySQL 연결 설정
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "a161319a!",
+  database: "db23208",
+});
+db.connect();
+module.exports = db;
