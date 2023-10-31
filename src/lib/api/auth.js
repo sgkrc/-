9afad4d2f4f37b183481615269544f7b76900c61
hopepisode <hopepisode@gmail.com>
2023-11-01 05:16:09 +0900

    클라이언트 api 부분에서 username을 params로 username을 받아서 username이란 이름으로 서버에 보냄.

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -4,14 +4,19 @@ import client from "./client";
 // 로그인
 export const login = ({ username, password }) =>
   client.post("/LogIn", { username, password });
-
 // 회원가입
 //post로 백엔드 서버의 /users로 보낸다는 것 ->서버에 따라 이부분을 변경
 export const register = ({ username, password, name, email }) =>
   client.post("/Register", { username, password, name, email });
 
 // 로그인 상태 확인
-export const check = () => client.get("/Welcome");
+export const check = (username) =>
+  client.get("/Welcome", {
+    params: {
+      //params 값으로 전달
+      username: username, // 사용자명 대신 실제 아이디 또는 사용자명 입력
+    },
+  });
 
 // 회원가입 상태 확인
 export const register_check = () => client.get("/SignUpComplete");
